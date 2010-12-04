2767b86ba7f9b2fc4986057c9d530cf840bfe116
Travis L Pinney <travis.pinney@gmail.com>
2010-12-04 14:48:03 -0500

    Added in density value for density json. Fixed up the density admin.

--- task/admin.py
+++ task/admin.py
@@ -1,10 +1,12 @@
 from django.contrib.gis import admin
 from models import Checkout, Density
 
-
+class DensityAdmin(admin.OSMGeoAdmin):
+    list_display = ('id','x','y','z','density')
+    
 
 
 admin.site.register(Checkout, admin.OSMGeoAdmin)
-admin.site.register(Density, admin.OSMGeoAdmin)
+admin.site.register(Density, DensityAdmin)
 
 
