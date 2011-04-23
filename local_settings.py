cf3f4676d7cf7820e31ba6af85ed9de1f6d99e38
Travis L Pinney <travis.pinney@gmail.com>
2011-04-23 16:10:57 -0400

    Adding derefenced settings.py with local_settings.py

---
+++ local_settings.py
@@ -0,0 +1,22 @@
+
+
+raise Exception("You need to modify your local_settings.py file")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'osmtask',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/Users/tlpinney/project/osmtask/templates'
+)
+
--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 # Django settings for osmtask project.
+import local_settings
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -8,16 +11,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'osmtask',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+DATABASES = local_settings.DATABASES
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -76,14 +70,12 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'osmtask.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    '/Users/tlpinney/project/osmtask/templates'
-)
+TEMPLATE_DIRS = local_settings.TEMPLATE_DIRS
 
 LOGIN_URL="accounts/login/"
+
+# You may new a GDAL_LIBRARY_PATH
+# GDAL_LIBRARY_PATH="/Library/Frameworks/GDAL.framework/unix/lib/libgdal.dylib"
 
 
 INSTALLED_APPS = (
@@ -96,8 +88,4 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-
-
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
 )
